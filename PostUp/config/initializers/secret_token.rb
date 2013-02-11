e30ae809f268a2888a5530ff93217c2bc51c5567
andrewrm <armiller5@gmail.com>
2013-02-11 15:13:18 -0700

    First commit. Rails environment is set up and nothing more

---
+++ PostUp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostUp::Application.config.secret_token = '6512be480d95252f4281dd4517ce0b04cf4edd795cd50fa9eca11ba2e0c7153a264df5c0d75600f0247902d8f6a849f5168bf74ed486e4d8f48ed371d1fad012'
---
+++ PostUp/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PostUp::Application.config.session_store :cookie_store, key: '_PostUp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PostUp::Application.config.session_store :active_record_store
