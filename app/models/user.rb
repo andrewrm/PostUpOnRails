13eabd70a02dd77ebbcfd1f12e0468bfc2b1fc74
andrewrm <armiller5@gmail.com>
2013-02-28 14:47:24 -0700

    Added email validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :geoposts
 
   validates(:name, presence: true, length: { maximum: 50 })
-  validates(:email, presence: true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX })
   #validates(:password, presence: true)
 end
